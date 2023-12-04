7494cedbc710969730dd8b4473fe1cf04de0d7c4
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-12-04 02:33:05 +0000

    test: add cases

--- __tests__/client.spec.ts
+++ __tests__/client.spec.ts
@@ -1,9 +1,10 @@
-import { createClient } from '@src/client.js'
+import { createBatchClient, createClient } from '@src/client.js'
 import { getErrorPromise } from 'return-style'
 import { WebSocketServer } from 'ws'
 import * as DelightRPCWebSocket from '@delight-rpc/websocket'
 import { ExtraNativeWebSocket } from 'extra-native-websocket'
 import { promisify } from 'extra-promise'
+import { createBatchProxy } from 'delight-rpc'
 
 interface IAPI {
   echo(message: string): string
@@ -44,6 +45,24 @@ describe('createClient', () => {
       await wsClient.close()
     }
   })
+  
+  test('echo (batch)', async () => {
+    const wsClient = new ExtraNativeWebSocket(() => new WebSocket(SERVER_URL))
+    await wsClient.connect()
+
+    try {
+      const [client, close] = createBatchClient(wsClient)
+      const proxy = createBatchProxy<IAPI>()
+
+      const result = await client.parallel(proxy.echo('hello'))
+      close()
+
+      expect(result.length).toBe(1)
+      expect(result[0].unwrap()).toBe('hello')
+    } finally {
+      await wsClient.close()
+    }
+  })
 
   test('error', async () => {
     const wsClient = new ExtraNativeWebSocket(() => new WebSocket(SERVER_URL))
@@ -53,6 +72,26 @@ describe('createClient', () => {
       const [client] = createClient<IAPI>(wsClient)
       const err = await getErrorPromise(client.error('hello'))
 
+      expect(err).toBeInstanceOf(Error)
+      expect(err!.message).toMatch('hello')
+    } finally {
+      await wsClient.close()
+    }
+  })
+
+  test('error (batch)', async () => {
+    const wsClient = new ExtraNativeWebSocket(() => new WebSocket(SERVER_URL))
+    await wsClient.connect()
+
+    try {
+      const [client, close] = createBatchClient(wsClient)
+      const proxy = createBatchProxy<IAPI>()
+
+      const result = await client.parallel(proxy.error('hello'))
+      close()
+
+      expect(result.length).toBe(1)
+      const err = result[0].unwrapErr()
       expect(err).toBeInstanceOf(Error)
       expect(err!.message).toMatch('hello')
     } finally {
